ceb99ac5be9dd8d8f1defcf6c3db3dff8aae1754
Areeba Huzaifa <162459943+areeba8820@users.noreply.github.com>
2024-07-10 15:55:07 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+// let n = +prompt("Enter the value of n")
+// let i = 0;
+// while(i<n){
+//     document.write(i)
+//     i++;
+// }
+
+// let n = +prompt("Enter the value of n")
+// let i = 0;
+// while(i<=n){
+    
+//     document.write("<br>" + i)
+//     i++;
+
+// }   
+
+// for(i=0; i >10; i++){
+//     console.log(i);
+// }
+
+// // write a program to display sum of numbers from 1 to 100
+
+// let sum = 0;
+// let num = 1;
+// while(num<=100){
+//     sum += num;   
+//     num++;
+// }
+// document.write("sum of 1 to 100: " + sum)
+
+// // sum = sum +num
+
+
+// write a program to display factorial of a number.
+
+// let number = 4;
+// let factorial = 1;
+// while(number>0){
+//     factorial = factorial * number;
+//     number--;
+// }
+// document.write("factorial of a number is: " + factorial)
+
+// write a program to display finding the first 10 even number
+
+// let evenNumber = []
+// let i = 1;
+// while(evenNumber.length<10){
+//     if(i%2  === 0){
+//         evenNumber.push(i)
+//     }
+//     i++
+
+// }
+// document.write("The first even number is: " + evenNumber)
+
+
+// write a progarm to display user input validation.
+
+// let userInput;
+// let password = "javascript123";
+// while(userInput!==password){
+//     userInput = prompt("Enter ypur Password");
+// }
+// document.write("Access Granted")
+
+// write a program to display generating countdown.
+
+// let countdown = 10;
+// while(countdown>0){
+//     document.write("<br>" + countdown)
+//     countdown--;
+// }
+// document.write("<br>" + "<h1>Happy New Year</h1>")
+
+
+let n = +prompt("Enter the value of n")
+let i = 10;
+do{
+    document.write("<br>" + i)
+    i++;
+}while(i<=n)
